3021dd0cce6331b2026f8abcfbcd620fc63c808b
Dennis Staack <151037774+DennisStaack@users.noreply.github.com>
2024-11-19 14:15:49 +0100

    Changed Calender for LRZ Gitlab

--- config.js
+++ config.js
@@ -71,9 +71,9 @@ const CONFIG = {
 		},
 		{
 			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
+			name: 'Gitlab',
+			icon: 'github',
+			link: 'https://gitlab.lrz.de/',
 		},
 		{
 			id: '5',
